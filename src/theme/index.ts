d984b23b97f74d19ffbf94f9e0231ef3f5088099
Ahmad Sabry <ahmadsabry330@gmail.com>
2021-10-13 00:29:45 +0200

    Added Formik & Yup

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -2,39 +2,12 @@ import { createTheme } from "@mui/material"
 
 const createMUITheme = () => {
     const theme = createTheme({
-        breakpoints: {
-            values: {
-                xs: 400,
-                sm: 600,
-                md: 980,
-                lg: 1024,
-                xl: 1200,
-            },
-        },
         palette: {
             primary: {
-                main: "#FF0000",
+                main: "#f16d36",
             },
             secondary: {
-                main: "#FFFFFF",
-            },
-        },
-        components: {
-            MuiTextField: {
-                styleOverrides: {
-                    root: {
-                        backgroundColor: "#FFFFFF",
-                        padding: "0px",
-                        margin: "0px",
-                    },
-                },
-            },
-            MuiTypography: {
-                styleOverrides: {
-                    subtitle1: {
-                        fontSize: "14px",
-                    },
-                },
+                main: "#387f94",
             },
         },
         typography: {
--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,17 +1,27 @@
 import { useState } from "react"
+import { Formik } from "formik"
+import * as Yup from "yup"
 
 import Grid from "@mui/material/Grid"
-import Button from "@mui/material/Button"
 import Container from "@mui/material/Container"
 import Typography from "@mui/material/Typography"
 import Checkbox from "@mui/material/Checkbox"
+import TextField from "@mui/material/TextField"
+import OutlinedInput from "@mui/material/OutlinedInput"
+import InputAdornment from "@mui/material/InputAdornment"
+import FormControl from "@mui/material/FormControl"
+import InputLabel from "@mui/material/InputLabel"
+import Button from "@mui/material/Button"
 
 import { items } from "appConstants"
 import { groupArray } from "helpers"
 import { Item } from "types/item"
+import { Box } from "@mui/system"
 
 const Home = () => {
     const [groupedItems, setGroupedItems] = useState(() => groupArray({ items }))
+    const [totalTax, setTotalTax] = useState<number | null>(null)
+    const [total, setTotal] = useState<number | null>(null)
 
     const handleItemToggle = ({ id, key }: { id: number; key: string }) => {
         const toggleditems = groupedItems[key].items.map((toggleItem: Item) => (toggleItem.id === id ? { ...toggleItem, selected: !toggleItem.selected } : toggleItem))
@@ -23,44 +33,140 @@ const Home = () => {
         setGroupedItems({ ...groupedItems, [key]: { items: toggleditems, selected: !groupedItems[key].selected } })
     }
 
+    const handleFormSubmit = async (values: any) => {
+        let currenttotalTax = 0
+        let currenttotal = 0
+
+        Object.keys(groupedItems).forEach((key) => {
+            groupedItems[key].items.forEach(({ selected, amount }: Item) => {
+                console.log("amount", amount, "selected", selected)
+                if (selected) {
+                    currenttotalTax += ((values?.taxValue || 0) / 100) * amount
+                }
+                currenttotal += ((values?.taxValue || 0) / 100) * amount
+            })
+        })
+
+        setTotalTax(currenttotalTax)
+        setTotal(currenttotal)
+    }
     return (
         <Container>
             <Typography>Add Tax</Typography>
 
-            {Object.keys(groupedItems).map((key) => {
-                return (
-                    <>
-                        <Grid container justifyContent="space-between">
-                            <Grid item>
-                                <Typography variant="h4">{key}</Typography>
-                            </Grid>
-                            <Grid item>
-                                <Button onClick={() => handleCategoryToggle({ key })}>toggle</Button>
-                            </Grid>
-                        </Grid>
-                        {groupedItems[key].items.map(({ name, selected, id }: Item) => {
-                            return (
-                                <Grid container justifyContent="space-between">
-                                    <Grid item>
-                                        <Checkbox checked={selected} />
-                                    </Grid>
-                                    <Grid item>
-                                        <Button onClick={() => handleItemToggle({ id, key })}>toggle</Button>
-                                    </Grid>
-                                    <Grid item>
-                                        <Typography>
-                                            {id} - {name}
-                                        </Typography>
-                                    </Grid>
-                                    <Grid item>
-                                        <Typography>{JSON.stringify(selected)}</Typography>
-                                    </Grid>
+            <Formik
+                onSubmit={handleFormSubmit}
+                initialValues={{
+                    taxName: "",
+                    taxValue: null,
+                }}
+                validationSchema={Yup.object().shape({
+                    taxName: Yup.string().required("Enter Tax Name"),
+                    taxValue: Yup.number().min(0).max(100).typeError("must be a number"),
+                })}
+            >
+                {({ errors, handleBlur, handleChange, handleSubmit, isSubmitting, touched, values }) => {
+                    return (
+                        <form noValidate onSubmit={handleSubmit}>
+                            <Grid container>
+                                <Grid item xs={12} sm={6}>
+                                    {/* taxName */}
+                                    <Box p={1}>
+                                        <TextField value={values.taxName} fullWidth name="taxName" disabled={isSubmitting} onChange={handleChange} onBlur={handleBlur} id="outlined-basic" label="Tax Name" />
+                                        {touched.taxName && errors.taxName && <Typography style={{ color: "red", fontSize: "12px" }}>{errors.taxName}</Typography>}
+                                    </Box>
                                 </Grid>
-                            )
-                        })}
-                    </>
-                )
-            })}
+
+                                <Grid item xs={12} sm={6}>
+                                    {/* taxValue */}
+                                    <Box p={1}>
+                                        <FormControl fullWidth variant="outlined">
+                                            <InputLabel htmlFor="outlined-adornment-password">Vat Value</InputLabel>
+                                            <OutlinedInput endAdornment={<InputAdornment position="end">%</InputAdornment>} type="number" value={values.taxValue} fullWidth name="taxValue" disabled={isSubmitting} onChange={handleChange} onBlur={handleBlur} id="outlined-basic" label="Tax Discount" />
+                                            {touched.taxValue && errors.taxValue && <Typography style={{ color: "red", fontSize: "12px" }}>{errors.taxValue}</Typography>}
+                                        </FormControl>
+                                    </Box>
+                                </Grid>
+                            </Grid>
+                            {Object.keys(groupedItems).map((key) => {
+                                return (
+                                    <>
+                                        <hr />
+                                        <Grid container alignItems="center">
+                                            <Grid item>
+                                                <Checkbox color="secondary" size="medium" onClick={() => handleCategoryToggle({ key })} checked={groupedItems[key].selected} />
+                                            </Grid>
+                                            <Grid item style={{ flexGrow: 1 }}>
+                                                <Typography variant="h4">{key}</Typography>
+                                            </Grid>
+                                            <Grid item xs={2}>
+                                                <Typography>Cost</Typography>
+                                            </Grid>
+                                            <Grid item xs={1}>
+                                                <Typography>Tax</Typography>
+                                            </Grid>
+                                        </Grid>
+                                        <hr />
+
+                                        {groupedItems[key].items.map(({ id, name, selected, amount }: Item) => {
+                                            return (
+                                                <Grid container alignItems="center">
+                                                    <Grid item>
+                                                        <Checkbox color="secondary" size="small" onClick={() => handleItemToggle({ id, key })} checked={selected} />
+                                                    </Grid>
+                                                    <Grid item style={{ flexGrow: 1 }}>
+                                                        <Typography>{name}</Typography>
+                                                    </Grid>
+                                                    <Grid item xs={2}>
+                                                        <Typography>{amount} $</Typography>
+                                                    </Grid>
+                                                    <Grid item xs={1}>
+                                                        <Typography>{selected ? (((values?.taxValue || 0) / 100) * amount).toFixed(2) : 0} $</Typography>
+                                                    </Grid>
+                                                </Grid>
+                                            )
+                                        })}
+                                    </>
+                                )
+                            })}
+
+                            <Grid sx={{ margin: "16px 0px" }} container>
+                                <Grid item sx={{ flexGrow: 1 }}>
+                                    {totalTax && (
+                                        <Typography variant="h4">
+                                            Total Taxs:<strong> {totalTax?.toFixed(2)} $</strong>
+                                        </Typography>
+                                    )}
+                                </Grid>
+
+                                <Grid item>
+                                    <Button type="submit" variant="contained">
+                                        Apply Tax to 6 item(s)
+                                    </Button>
+                                </Grid>
+                            </Grid>
+                            <Grid sx={{ margin: "16px 0px" }} container>
+                                <Grid item sx={{ flexGrow: 1 }}>
+                                    {total && (
+                                        <Typography variant="h4">
+                                            Total Item: <strong>{total?.toFixed(2)} $</strong>
+                                        </Typography>
+                                    )}
+                                </Grid>
+                            </Grid>
+                            <Grid sx={{ margin: "16px 0px" }} container>
+                                <Grid item sx={{ flexGrow: 1 }}>
+                                    {total && totalTax && (
+                                        <Typography variant="h4">
+                                            Total: <strong>{(total + totalTax).toFixed(2)} $</strong>
+                                        </Typography>
+                                    )}
+                                </Grid>
+                            </Grid>
+                        </form>
+                    )
+                }}
+            </Formik>
         </Container>
     )
 }
